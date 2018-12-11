28b0f3369d25de9d0e2f788786ebbc8406616ee1
Rafael Coutinho <rafacout@gmail.com>
2018-12-11 15:04:26 -0200

    Implemented the menu and reviews

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import {Observable} from 'rxjs/Observable';
 import {ErrorHandler} from '../app.error-handler';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -24,4 +25,15 @@ export class RestaurantsService {
         .catch(ErrorHandler.handleError);
     }
 
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
 }
