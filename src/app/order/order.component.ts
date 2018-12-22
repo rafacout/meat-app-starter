5a560c23631a529ba203dda1e2377843d886c965
Rafael Coutinho <rafacout@gmail.com>
2018-12-22 11:58:45 -0200

    Added the rating component

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
@@ -19,7 +20,7 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão Refeição', value: 'REF' }
   ];
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -50,7 +51,7 @@ export class OrderComponent implements OnInit {
 
     this.orderService.checkOrder(order)
     .subscribe((orderId: string) => {
-      console.log(`Compra concluida:  ${orderId}`);
+      this.router.navigate(['/order-summary']);
       this.orderService.clear();
     });
 
