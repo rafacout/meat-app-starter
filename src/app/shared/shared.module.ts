3de4188dbfe0919d94065116d5a4003456a9af9b
Rafael Coutinho <rafacout@gmail.com>
2018-12-27 17:09:40 -0200

    Changed the Core Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -6,11 +6,24 @@ import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
 import { RatingComponent } from './rating/rating.component';
 
+import { OrderService } from "../order/order.service";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+
+
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
     exports: [InputComponent, RadioComponent, RatingComponent, 
-        CommonModule, FormsModule, ReactiveFormsModule]
+              CommonModule, FormsModule, ReactiveFormsModule]
 })
 
-export class SharedModule {}
+export class SharedModule {
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantsService, OrderService]
+        }
+    }
+}
