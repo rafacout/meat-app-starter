ea00b6c7f16ce4ce101c63623c76b0ed1a80accd
Rafael Coutinho <rafacout@gmail.com>
2018-12-27 12:02:04 -0200

    Changed the about to be a Module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import {Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component';
 import {MenuComponent} from './restaurant-detail/menu/menu.component';
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
 import {OrderComponent} from './order/order.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import {OrderSummaryComponent} from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
     {path: '', component : HomeComponent},
@@ -20,5 +19,5 @@ export const ROUTES: Routes = [
         ]},
     {path: 'order', component : OrderComponent},
     {path: 'order-summary', component : OrderSummaryComponent},
-    {path: 'about', component : AboutComponent}
+    {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ];
