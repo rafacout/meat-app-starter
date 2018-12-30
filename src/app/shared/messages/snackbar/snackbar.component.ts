a920957ab73905ec93140cadf8a611e09ca8d2f3
Rafael Coutinho <rafacout@gmail.com>
2018-12-30 11:06:40 -0200

    Animations

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate} from "@angular/animations";
 
-import { NotificationService } from '../notification.service';
-import { Observable } from 'rxjs';
+import {NotificationService} from '../notification.service';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
@@ -19,24 +21,26 @@ import 'rxjs/add/observable/timer';
         opacity: 1,
         bottom: '30px'
       })),
-      transition('hidden => visible', animate('500mx 0s ease-in')),
-      transition('visible => hidden', animate('500mx 0s ease-out'))
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
     ])
   ]
 })
 export class SnackbarComponent implements OnInit {
 
-  message: string = 'Hello there!';
+  message: string;
+
   snackVisibility: string = 'hidden';
 
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
-    });
+    this.notificationService.notifier
+    .do(message => {
+        this.message = message;
+        this.snackVisibility = 'visible';
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer=> this.snackVisibility = 'hidden');
   }
 
 }
