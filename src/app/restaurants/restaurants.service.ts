cad3aa1ed3262ded2ab76e4a1f324c8bd1d3bd28
Rafael Coutinho <rafacout@gmail.com>
2019-08-18 12:49:30 -0300

    Criado o componente de restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,7 +3,9 @@ import { MEAT_API } from "../app.api";
 import { Http } from "@angular/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
+import { ErrorHandler } from "../app.error-handler";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 
 @Injectable()
@@ -14,10 +16,10 @@ export class RestaurantService {
         private http: Http
     ){}
 
-
-
     restaurants(): Observable<Restaurant[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
-        .map(response => response.json());
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
     }
+
 }
