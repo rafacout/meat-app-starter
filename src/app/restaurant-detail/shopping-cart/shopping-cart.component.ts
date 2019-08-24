b81c430cb6de6818e98ec21da6ef660064038455
Rafael Coutinho <rafacout@gmail.com>
2019-08-24 12:19:36 -0300

    Ajuste no adicionar items

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -22,4 +22,12 @@ export class ShoppingCartComponent implements OnInit {
   total(): number {
     return this.shoppingCartService.total();
   }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item);
+  }
 }
